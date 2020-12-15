5c97bc3895874a0c606e15fcd4e709a1a893e993
R0b1n-s <75830126+R0b1n-s@users.noreply.github.com>
2020-12-15 11:17:17 +0100

    Jeux carte version qui marche
    Parce que Thomas cette autiste autiste autiste est incapable de ce relire et de corriger des fautes simple

--- JeuCartes.py
+++ JeuCartes.py
@@ -1,53 +1,52 @@
-﻿from valeursglobales import *
+﻿from carte import *
 import random
 
 
 class JeuCartes() :
-    def _init_(self, nbCartes=52):
+    def __init__(self, nbCartes=52):
         self.nbCartes = nbCartes
         self.jeu = []
         self.creerJeu()
 
 
-def getTailleJeu(self):
-    return self.nbCartes
+    def getTailleJeu(self):#Renvoie la taille du jeu
+        return self.nbCartes
 
-def creerJeu(self):
-    for couleur in couleurs:
-        for nom in noms:
-            if (self.nbCartes == 32 and nom not in ['2','3','4','5','6']):
-                self.jeu.append(Carte(nom, couleur))
-            if (self.nbCartes == 52):
-                self.jeu.append(Carte(nom, couleur))
+    def creerJeu(self):
+        for couleur in couleurs:#Creer un jeu entier
+            for nom in noms:
+                if (self.nbCartes == 32 and nom not in ['2','3','4','5','6']):
+                    self.jeu.append(Carte(nom, couleur))
+                if (self.nbCartes == 52):
+                    self.jeu.append(Carte(nom, couleur))
 
-def getJeu(self):
-    return self.jeu
+    def getJeu(self):
+        return self.jeu#Rencoie le jeu
 
-def melanger(self):
-    return random.shuffle(self.jeu)
+    def melanger(self):
+        return random.shuffle(self.jeu)#Melange le jeu
 
-def distribuerCarte(self):
-    derniereCarte= (self.jeu[-1])
-    self.jeu.pop()
-    return derniereCarte
+    def distribuerCarte(self):
+        derniereCarte= (self.jeu[-1])
+        self.jeu.pop()
+        return derniereCarte#Renvoie la dernière carte de la main
 
-def distribuerJeu(self, nbJoueurs, nbCartes):
-    if (nbJoueurs*nbCartes<=self.nbCartes):
-        tableau=[]
-        for i in range(nbJoueurs):
-            tableau.append([])
-            for j in range(nbCartes):
-                tableau[i].append(self.distribuerCarte())
-        return tableau
-    else:
-        print("Pas assez de cartes dans le jeu")
+    def distribuerJeu(self, nbJoueurs, nbCartes):#Redistribue les cartes aux joueur
+        if (nbJoueurs*nbCartes<=self.nbCartes):
+            tableau=[]
+            for i in range(nbJoueurs):
+                tableau.append([])
+                for j in range(nbCartes):
+                    tableau[i].append(self.distribuerCarte())
+            return tableau
+        else:
+            print("Pas assez de cartes dans le jeu")
 
 def testJeuCartes():
     jeu52 = JeuCartes(52)
-    jeu52.melanger
-
-    L=jeu52.getJeu()
-    carte = L[2]
+    jeu52.melanger()
+    carte = Carte('2','pique')
+    jeu52.getJeu()
     print('Nom:', carte.getNom())
     print('Couleur:', carte.getNom())
     print('Valeur:', carte.getNom())
