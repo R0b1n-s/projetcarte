6724a7640992522978b8c5d412d0f20f2f945a93
R0b1n-s <75830126+R0b1n-s@users.noreply.github.com>
2020-12-15 11:02:35 +0100

    Class Joueur pas complète
    Il faut la finalisé avec la class JeuCarte

--- Joueur.py
+++ Joueur.py
@@ -1,20 +1,47 @@
-#Class de création d'un joueur
-"""Import jeuxcartes"""
-import carte
-nom = ""
-# JeuxCarte = jeuxcarte(nbCarte=52)
-
-carte = carte("Dame","Carreaux")
-
-class mains:
-    def __init__(self,carte):
-        self.carte = carte
-
-
-
-class Joueur:
-    def __init__(self,nom,nbCartes,mainJoueur):
+from carte import *
+#from jeuxCarte import *
+class Joueur():
+    def __init__(self, nom,nbCarte,mainJoueur):
         self.nom = nom
-        #self.nbCarte = JeuxCarte.getTailleDeJeu()
+        self.nbCartes = nbCarte
+        self.mainJoueur = mainJoueur
 
 
+    def setMain(self,listeCartes):
+
+        self.mainJoueur = listeCartes
+        self.nbCartes= self.nbCartes + len(self.mainJoueur)
+
+    def getNom(self):
+
+        return self.nom
+
+    def getNbCartes(self):
+
+        return self.nbCartes
+
+    def jouerCarte(self):
+
+        derniereCarte= (self.mainJoueur[-1])
+        if (derniereCarte == []):
+            return "None"
+        else:
+            self.mainJoueur.pop()
+            self.nbCartes = self.nbCartes - 1
+            return derniereCarte
+
+    def insererMain(self,CartesGagnees):
+
+        for liste in CartesGagnees:
+            self.mainJoueur.insert(0,liste)
+            self.nbCartes = self.nbCartes +1
+        return self.mainJoueur
+
+
+def test():
+    a =[1,2,3]
+    _joueur = Joueur("Rob",32,a)
+    _joueur.setMain(a)
+    print(_joueur.getNom())
+    print(_joueur.getNbCartes())
+    print(_joueur.jouerCarte())
